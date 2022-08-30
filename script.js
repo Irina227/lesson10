b7e0743fce819f9bceafbe15c816d47355d5b37d
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-30 10:36:58 +0300

    урок 10 домашка

--- script.js
+++ script.js
@@ -3,14 +3,18 @@
 /*УРОК 10*/
 const books = document.querySelectorAll(".book"); //обращение к книге, меняются местами книги
 const elem = document.querySelectorAll("ul");
-
+const list = document.querySelectorAll("li");
 books[0].before(books[1]);
 books[4].after(books[3]);
 books[5].after(books[2]);
-
 books[0].querySelector("ul");
-books[0].append(elem[3]);
 
+
+
+
+console.log(books[0].querySelector('ul'));
+console.log(books[0].querySelector('ul').querySelectorAll('li'));
+console.log(books[0].querySelector('ul').querySelectorAll('li')[2]);
 console.log(books);
 console.log(books[0]);
 
@@ -21,6 +25,8 @@ google[0].remove();
 
 const listing = document.querySelectorAll("a"); //передача новой строки элементу
 listing[2].replaceWith("Книга 3. this и Прототипы Объектов");
+
+// books[0].elem[1].after(list[2])
 
 // const href = document.querySelectorAll('ul');//обращение к списку
 // const elems = document.querySelectorAll('li');//обращение к элементу списка
