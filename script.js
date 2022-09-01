ec3a8708d7b75610aed64cbc09979b2802ce1f11
Ira Korytko <irichkakorytko@yandex.ru>
2022-09-01 10:33:13 +0300

    урок 10

--- script.js
+++ script.js
@@ -4,10 +4,33 @@
 const books = document.querySelectorAll(".book"); //обращение к книге, меняются местами книги
 const elem = document.querySelectorAll("ul");
 const list = document.querySelectorAll("li");
+
 books[0].before(books[1]);
 books[4].after(books[3]);
 books[5].after(books[2]);
-books[0].querySelector("ul");
+
+books[0].querySelector("ul").before(list[0]);
+books[0].querySelector("ul").before(list[1]);
+books[0].querySelector("ul").before(list[3]);
+books[0].querySelector("ul").before(list[6]);
+books[0].querySelector("ul").before(list[8]);
+books[0].querySelector("ul").before(list[4]);
+books[0].querySelector("ul").before(list[5]);
+books[0].querySelector("ul").before(list[7]);
+books[0].querySelector("ul").before(list[9]);
+books[0].querySelector("ul").before(list[2]);
+books[0].querySelector("ul").before(list[10]);
+
+// books[5].querySelector("ul").before(list[1]);
+// books[5].querySelector("ul").before(list[9]);
+// books[5].querySelector("ul").before(list[3]);
+// books[5].querySelector("ul").before(list[4]);
+// books[5].querySelector("ul").before(list[2]);
+// books[5].querySelector("ul").before(list[6]);
+// books[5].querySelector("ul").before(list[7]);
+// books[5].querySelector("ul").before(list[5]);
+// books[5].querySelector("ul").before(list[8]);
+
 
 
 
@@ -26,15 +49,4 @@ google[0].remove();
 const listing = document.querySelectorAll("a"); //передача новой строки элементу
 listing[2].replaceWith("Книга 3. this и Прототипы Объектов");
 
-// books[0].elem[1].after(list[2])
 
-// const href = document.querySelectorAll('ul');//обращение к списку
-// const elems = document.querySelectorAll('li');//обращение к элементу списка
-
-// href[4].prepend(elems[1]);
-// console.log(elems);
-
-// list[6].replaceWith('Книга 3. this и Прототипы Объектов');
-// list[2].append('Глава 8: За пределами ES6');
-
-// console.log(list);
